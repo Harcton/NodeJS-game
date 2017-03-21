dad1a853a477bbb80d4fb5f4c1fd4b4017e3e14f
Teo Hiltunen <pragux@gmail.com>
2017-03-21 12:08:06 +0200

    Class hierarchy added

--- app.js
+++ app.js
@@ -10,42 +10,202 @@ app.get("/", function(req, res) {
 app.use("/client", express.static(__dirname + "/client"));
 
 //Start the server
-serv.listen(2000);
+serv.listen(2001);
 console.log("Server started");
 
 var SOCKET_LIST = {};
-var PLAYER_LIST = {};
-var currentSocketID = 1;
+var ENTITY_LIST = {};
+var currentSocketID = 1;//Positive direction
+var currentNPCID = -1;//Negative direction
 
-var Player = function(id) {
-	var self = {
-		x:250,
-		y:250,
-		id:id,
-		number:"" + id,
-		pressingRight:false,
-		pressingLeft:false,
-		pressingUp:false,
-		pressingDown:false,
-		maxSpd:10,
+class Entity
+{
+	constructor(_id, _x, _y)
+	{
+		console.log("ENTITY CONSTRUCTOR");
+		this.id = _id;
+		this.x = 0;
+		this.y = 0;
+	}
+	onDestroy()
+	{
+		console.log("ENTITY DESTRUCTOR");
+	}
+	update(deltaTime)
+	{
+		//console.log("ENTITY UPDATE");
+		return true;
+	}
+}
+
+class Arrow extends Entity
+{
+	constructor(_id, _x, _y, _damage, _direction)
+	{
+		super(_id, _x, _y);
+		this.direction = _direction;
+	}
+	onDestroy()
+	{
+		super.onDestroy();
+		console.log("ARROW DESTRUCTOR");
+	}
+	update(deltaTime)
+	{
+		if (!super.update(deltaTime))
+			return false;
+		
+		this.x += Math.cos(this.direction) * 1.0;
+		this.y += Math.cos(this.direction) * 1.0;
+		
+		return true;
+	}
+}
+
+class Bomb extends Entity
+{
+	constructor(_id, _x, _y, _damage, _timer)
+	{
+		super(_id, _x, _y);
+		this.damage = _damage;
+		this.timer = _timer;
+	}
+	onDestroy()
+	{
+		super.onDestroy();
+		console.log("BOMB DESTRUCTOR");
+	}
+	update(deltaTime)
+	{
+		if (!super.update(deltaTime))
+			return false;
+		
+		//Decrement timer
+		this.timer -= deltaTime;
+		if (this.timer <= 0)
+			return false;
+		
+		return true;
+	}
+}
+
+class Character extends Entity
+{
+	constructor(_id, _x, _y, _profession)
+	{		
+		super(_id, _x, _y);
+		console.log("CHARACTER CONSTRUCTOR: " + _profession);
+		this.profession = _profession;
+		this.name = _profession;
+		this.attackTimer = 0.0;		
+		
+		//Profession specific attributes
+		switch (this.profession)
+		{
+			case "Archer":
+				this.attack = 1;
+				this.rate = 1;
+				this.speed = 1.0;
+				this.health = 1;
+				this.regeneration = 1;
+				this.projectileRes = 1;
+				this.bombRes = 1;
+				this.meleeRes = 1;
+				break;
+			case "Bomber":
+				this.attack = 1;
+				this.rate = 1;
+				this.speed = 1.0;
+				this.health = 1;
+				this.regeneration = 1;
+				this.projectileRes = 1;
+				this.bombRes = 1;
+				this.meleeRes = 1;
+				break;
+			case "Crusader":
+			
+				this.attack = 1;
+				this.rate = 1;
+				this.speed = 1.0;
+				this.health = 1;
+				this.regeneration = 1;
+				this.projectileRes = 1;
+				this.bombRes = 1;
+				this.meleeRes = 1;
+				break;
+		}
+	}
+	onDestroy()
+	{
+		super.onDestroy();
+		console.log("CHARACTER DESTRUCTOR");
 	}
 	
-	self.updatePosition = function(){
-		if (self.pressingRight) {
-			self.x += self.maxSpd;
-		}
-		if (self.pressingLeft) {
-			self.x -= self.maxSpd;
-		}
-		if (self.pressingUp) {
-			self.y -= self.maxSpd;
-		}
-		if (self.pressingDown) {
-			self.y += self.maxSpd;
-		}
+	update(deltaTime)
+	{
+		if (!super.update(deltaTime))
+			return false;
+		
+		return true;
 	}
+}
+
+class Player extends Character
+{
+	constructor(_id, _x, _y, _profession, _name)
+	{
+		super(_id, _x, _y, _profession);
+		console.log("PLAYER CONSTRUCTOR: " + _name);
+		this.name = _name;
+		this.number = "" + this.id;
+		this.pressingRight = false;
+		this.pressingLeft = false;
+		this.pressingUp = false;
+		this.pressingDown = false;
+	}
+	onDestroy()
+	{
+		super.onDestroy();
+		console.log("PLAYER DESTRUCTOR");
+	}	
 	
-	return self;
+	update(deltaTime)
+	{
+		if (!super.update(deltaTime))
+			return false;
+		
+		if (this.pressingRight)
+			this.x += this.speed;
+		if (this.pressingLeft)
+			this.x -= this.speed;
+		if (this.pressingUp)
+			this.y -= this.speed;
+		if (this.pressingDown)
+			this.y += this.speed;
+		
+		console.log("Position: " + this.x + ", " + this.y);		
+		return true;
+	}
+}
+
+class Enemy extends Character
+{
+	constructor(_id, _x, _y, _profession)
+	{
+		super(_id, _x, _y, _profession);
+	}
+	onDestroy()
+	{
+		super.onDestroy();
+		console.log("ENEMY DESTRUCTOR");
+	}
+	update(deltaTime)
+	{
+		if (!super.update(deltaTime))
+			return false;
+		
+		return true;
+	}
 }
 
 var io = require("socket.io") (serv, {});
@@ -53,36 +213,47 @@ io.sockets.on("connection", function(socket){
 	
 	socket.id = currentSocketID++;
 	SOCKET_LIST[socket.id] = socket;
-	
-	var player = Player(socket.id);
-	PLAYER_LIST[socket.id] = player;
+	var player = new Player(socket.id, 0, 0, "Archer", "Dummy");
 	
 	socket.on("disconnect", function(){
 		delete SOCKET_LIST[socket.id];
-		delete PLAYER_LIST[socket.id];
+		var entity = ENTITY_LIST[socket.id];//Cast to entity
+		entity.onDestroy();
+		delete ENTITY_LIST[socket.id];
+	});
+	
+	socket.on("spawn", function(buffer){
+		console.log("Spawning player...");
+		player = new Player(socket.id, 0, 0, buffer.profession, buffer.name);
+		ENTITY_LIST[socket.id] = player;
 	});
 	
 	socket.on("u", function(buffer){
-		console.log(buffer[0]);
+		//console.log(buffer[0]);
 		player.pressingRight	= ((buffer[0] & 1) !== 0);
 		player.pressingUp		= ((buffer[0] & 2) !== 0);
 		player.pressingLeft		= ((buffer[0] & 4) !== 0);
 		player.pressingDown		= ((buffer[0] & 8) !== 0);
 	});
-	
 })
-
 
 //Update and send
 setInterval(function(){
 	var packet = [];
-	for (var i in PLAYER_LIST){
-		var player = PLAYER_LIST[i];
-		player.updatePosition();
+	for (var i in ENTITY_LIST)
+	{
+		var entity = ENTITY_LIST[i];
+		
+		if (!entity.update(1000.0/25.0))
+		{
+			ENTITY_LIST[entity.id].onDestroy();
+			delete ENTITY_LIST[entity.id];
+		}
+		
 		packet.push({
-			x:player.x,
-			y:player.y,
-			number:player.number
+			x:entity.x,
+			y:entity.y,
+			number:entity.id
 		});
 	}
 	

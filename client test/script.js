408595bb8e91d5d5daaa5893773366c3cf855dfc
Yuuso <juusoturunen94@hotmail.com>
2017-03-28 11:06:23 +0300

    generalized object

--- client test/script.js
+++ client test/script.js
@@ -39,103 +39,60 @@ $(document).ready(function()
 		{
 			this.x = 0;
 			this.y = 0;
-			this.xd = 0;
-			this.yd = 0;
+			this.xlook = 0;
+			this.ylook = 0;
 			this.scale = 0.5;
 
 			this.update = function()
 			{
-				//this.xd = (mouseX - canvas.width / 2) / 4;
-				//this.yd = (mouseY - canvas.height / 2) / 4;
+				this.xlook = (mouseX - canvas.width / 2) / 4;
+				this.ylook = (mouseY - canvas.height / 2) / 4;
 
-				this.x = object.x * camera.scale - canvas.width / 2 + this.xd;
-				this.y = object.y * camera.scale - canvas.height / 2 + this.yd;
+				this.x = object.x * camera.scale - canvas.width / 2 + this.xlook;
+				this.y = object.y * camera.scale - canvas.height / 2 + this.ylook;
 			};
 		};
 
-		var object = new function()
+		function SpriteObject(_sprite, _x, _y)
 		{
-			this.x = 0;
-			this.y = 0;
-			this.xs = 0;
-			this.ys = 0;
-			this.r = 0;
-			this.ball = new Image();
-			this.indicator = new Image();
-
-			this.ball.src = "ball.png";
-			this.indicator.src = "indicator.png";
+			this.x = _x;
+			this.y = _y;
+			this.xspeed = 0;
+			this.yspeed = 0;
+			this.rotation = 0;
+			this.sprite = new Image();
+			this.sprite.src = _sprite;
 			
 			this.draw = function()
 			{
-				ctx.translate(-camera.x + (this.x - this.ball.width / 2) * camera.scale, -camera.y + (this.y - this.ball.height / 2) * camera.scale);
-				ctx.rotate(this.r);
-
-				ctx.drawImage(this.ball, -this.ball.width / 2 * camera.scale, -this.ball.height / 2 * camera.scale,
-				 this.ball.width * camera.scale, this.ball.height * camera.scale);
-				ctx.drawImage(this.indicator, -this.ball.width / 2 * camera.scale, -this.ball.height / 2 * camera.scale,
-				 this.indicator.width * camera.scale, this.indicator.height * camera.scale);
-
-				ctx.rotate(-this.r);
-				ctx.translate(-(-camera.x + (this.x - this.ball.width / 2) * camera.scale), -(-camera.y + (this.y - this.ball.height / 2) * camera.scale));
-			};
-			this.update = function()
-			{
-				this.x += this.xs;
-				this.y += this.ys;
-				this.r = Math.atan2(mouseY - (this.y * camera.scale - camera.y), mouseX - (this.x * camera.scale - camera.x));
-			};
-		};
-
-		var bullets = [];
-		function Bullet(xspeed, yspeed, posx, posy)
-		{
-			this.x = posx;
-			this.y = posy;
-			this.xs = xspeed;
-			this.ys = yspeed;
-
-			this.bullet = new Image();
-
-			this.bullet.src = "bullet.png";
-
-			this.draw = function()
-			{
-				ctx.translate(-camera.x + this.x, -camera.y + this.y);
-				ctx.drawImage(this.bullet, -this.bullet.width / 2 * camera.scale, -this.bullet.height / 2 * camera.scale,
-				 this.bullet.width * camera.scale, this.bullet.height * camera.scale);
-				ctx.translate(-(-camera.x + this.x), -(-camera.y + this.y));
-			};
-			this.update = function()
-			{
-				this.x += this.xs;
-				this.y += this.ys;
-			};
-		};
-
-		var markers = [];
-		function Marker(posx, posy)
-		{
-			this.x = posx;
-			this.y = posy;
-
-			this.marker = new Image();
-			this.marker.src = "marker.png";
-
-			this.draw = function()
-			{
 				ctx.translate(-camera.x + this.x * camera.scale, -camera.y + this.y * camera.scale);
-				ctx.drawImage(this.marker, -this.marker.width / 2 * camera.scale, -this.marker.height / 2 * camera.scale,
-				 this.marker.width * camera.scale, this.marker.height * camera.scale);
+				ctx.rotate(this.rotation);
+
+				ctx.drawImage(this.sprite, -this.sprite.width / 2 * camera.scale, -this.sprite.height / 2 * camera.scale,
+				 this.sprite.width * camera.scale, this.sprite.height * camera.scale);
+
+				ctx.rotate(-this.rotation);
 				ctx.translate(-(-camera.x + this.x * camera.scale), -(-camera.y + this.y * camera.scale));
 			};
-		}
+			this.update = function()
+			{
+				this.x += this.xspeed;
+				this.y += this.yspeed;
+				this.rotation = Math.atan2(mouseY - (this.y * camera.scale - camera.y), mouseX - (this.x * camera.scale - camera.x));
+			};
+		};
+
+		var object = new SpriteObject("ball.png", 0, 0);
+
+		var bullets = [];
+
+		var markers = [];
 		var markerDistance = 400;
 		for (var x = -10; x < 10; x++)
 		{
 			for (var y = -10; y < 10; y++)
 			{
-				markers.push(new Marker(x * markerDistance, y * markerDistance));
+				markers.push(new SpriteObject("marker.png", x * markerDistance, y * markerDistance));
 			}
 		}
 
@@ -166,32 +123,36 @@ $(document).ready(function()
 			window.onkeydown = function(event)
 			{
 				if (event.keyCode == 65) //a
-					object.xs = -speed;
+					object.xspeed = -speed;
 				if (event.keyCode == 68) //d
-					object.xs = speed;
+					object.xspeed = speed;
 				if (event.keyCode == 83) //s
-					object.ys = speed;
+					object.yspeed = speed;
 				if (event.keyCode == 87) //w
-					object.ys = -speed;
+					object.yspeed = -speed;
 
 				if (event.keyCode == 32) //space
-					bullets.push(new Bullet(bulletSpeed * Math.cos(object.r), bulletSpeed * Math.sin(object.r), object.x, object.y));
+				{
+					bullets.push(new SpriteObject("bullet.png", object.x, object.y));
+					bullets[bullets.length - 1].xspeed = bulletSpeed * Math.cos(object.rotation);
+					bullets[bullets.length - 1].yspeed = bulletSpeed * Math.sin(object.rotation);
+				}
 			}
 			window.onkeyup = function(event)
 			{
 				if (event.keyCode == 65) //a
-					object.xs = 0; 
+					object.xspeed = 0; 
 				if (event.keyCode == 68) //d
-					object.xs = 0;
+					object.xspeed = 0;
 				if (event.keyCode == 83) //s
-					object.ys = 0;
+					object.yspeed = 0;
 				if (event.keyCode == 87) //w
-					object.ys = 0;
+					object.yspeed = 0;
 			}
 			canvas.onmousemove = function(event)
 			{
-				mouseX = event.pageX;
-				mouseY = event.pageY;
+				mouseX = event.offsetX;
+				mouseY = event.offsetY;
 			}
 			canvas.onmousewheel = function(event)
 			{
@@ -208,17 +169,20 @@ $(document).ready(function()
 				mouseW *= 0.9;
 			}
 			camera.scale += mouseW * 0.00015;
-			if (camera.scale < 0.1)
-				camera.scale = 0.1;
+			var maxZoom = 0.05;
+			if (camera.scale < maxZoom)
+				camera.scale = maxZoom;
 
 			object.update();
 			camera.update();
 			for(var i = 0; i < bullets.length; i++)
 			{
-				var debugtest = document.getElementById("debugtext");
-				debugtext.innerHTML = "bullets: " + bullets.length;
 				bullets[i].update();
 			}
+
+				var debugtest = document.getElementById("debugtext");
+				debugtext.innerHTML = "mouseX: " + mouseX + ", mouseY: " + mouseY;
+
 			window.requestAnimationFrame(update);
 		}
 
